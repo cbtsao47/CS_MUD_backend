2809b5023c8a442bdd073ec7471c5b36530b895f
curtis <chubbard022@gmail.com>
2019-07-29 11:26:15 -0700

    making migrations to the room,player, and map

--- map/models.py
+++ map/models.py
@@ -7,6 +7,7 @@ This file contains the classes for the map layout.
 from django.db import models
 
 class Map(models.Model):
+    pass
     #auto increment id
     #has to connect to rooms
 
--- room/models.py
+++ room/models.py
@@ -9,5 +9,6 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
+    pass
     #Possible foreign key that connects each room
     #room number
