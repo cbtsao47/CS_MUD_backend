5fb57253494abf950a0fe7173c583c9796439a15
Ben <cbtsao47@gmail.com>
2019-07-29 18:45:24 -0500

    update allowed hosts

--- mud_backend/settings.py
+++ mud_backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['cs-mud-backend.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'lambda-mud-test-app.herokuapp.com/', 'cs-mud-backend.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
