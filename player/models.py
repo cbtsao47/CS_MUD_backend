798352aa2f9fb18fd7bb86a64db5d6cadd6f1016
curtis <chubbard022@gmail.com>
2019-07-29 11:18:54 -0700

    creating a player class that has id and name field

--- player/models.py
+++ player/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+"""
 
-# Create your models here.
+This file contains the classes for the player layout. 
+
+Each player is given an auto generated id from the UUID library
+    The ID is also each players primary key
+Each player can give themselves a name that is less than or equal to fifty characters
+
+"""
+
+from django.db import models
+from uuid import uuid4
+
+class Player(models.Model):
+    id = models.UUIDField(primary_key=True,default=uuid4, editable=False)
+    name = models.CharField(max_length=50,blank=False)
+    #current room
