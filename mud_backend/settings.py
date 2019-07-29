4a05ccfc3d8b8122f2856eed039331f846210c2d
curtis <chubbard022@gmail.com>
2019-07-29 10:32:01 -0700

    installing graphql for backend

--- mud_backend/settings.py
+++ mud_backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "graphene_django",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
